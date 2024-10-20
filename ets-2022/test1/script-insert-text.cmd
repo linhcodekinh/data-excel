b71b6682c059044f18d999d33fc27228fba14b66
linhcodekinh <nvlinh296@gmail.com>
2024-10-20 17:38:06 +0700

    data est-2022

---
+++ ets-2022/test1/script-insert-text.cmd
@@ -0,0 +1,9 @@
+tesseract 4.png stdout -l eng >> test1.txt
+tesseract 5.png stdout -l eng >> test1.txt
+tesseract 6.png stdout -l eng >> test1.txt
+tesseract 7.png stdout -l eng >> test1.txt
+tesseract 8.png stdout -l eng >> test1.txt
+tesseract 9.png stdout -l eng >> test1.txt
+tesseract 10.png stdout -l eng >> test1.txt
+tesseract 11.png stdout -l eng >> test1.txt
+tesseract 12.png stdout -l eng >> test1.txt
